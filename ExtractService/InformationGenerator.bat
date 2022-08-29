8dabf17c6df199dee52890017cafe66d24c80740
steambird1 <steambird@163.com>
2022-08-29 17:09:47 +0800

    Fix bug in generator

--- ExtractService/InformationGenerator.bat
+++ ExtractService/InformationGenerator.bat
@@ -1,6 +1,12 @@
 @echo off
+setlocal enabledelayedexpansion
 rem This document can help generate ExtractList.txt.
 set /p begin="Input directory: "
-for /F "tokens=*" %%i in ('dir /s /b %begin%') do echo %%~nxi ^> ^\ >> ExtractList.txt
-echo Completed!
+mkdir output
+for /F "tokens=*" %%i in ('dir /s /b %begin%') do (
+	set myname=%random%_%%~nxi
+	echo !myname! ^> ^\ >> ExtractList.txt
+	copy %%~i output\!myname!
+)
+echo Completed! Please compress files in %~dp0\output.
 pause
